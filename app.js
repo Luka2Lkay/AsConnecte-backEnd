6b7a5610eb531ba397aa6b310b2c94b22a356c71
Luka <lukhanyo95@gmail.com>
2023-11-04 16:03:54 +0200

    verify token

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const port = 3300;
 const resellerRouter = require('./src/routes/reseller_routes.js');
 const { db } = require("./src/config/db_config");
 
-
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
