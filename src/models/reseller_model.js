9e191dd7f470daf7885b6087953d27e429b69ea2
Luka <lukhanyo95@gmail.com>
2023-11-19 16:27:32 +0200

    modify the models

--- src/models/reseller_model.js
+++ src/models/reseller_model.js
@@ -4,8 +4,8 @@ const uniqueValidator = require("mongoose-unique-validator");
 const resellerSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
-  confirmPassword: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
+  confirmPassword: { type: String, required: true},
   wifiDetails: [{type: mongoose.Types.ObjectId, ref: "WifiDetails"}]
 });
 
--- src/models/wifi_details_model.js
+++ src/models/wifi_details_model.js
@@ -3,9 +3,9 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const wifiDetailsSchema = new mongoose.Schema({
   wifiName: { type: String, required: true, unique: true },
-  wifiProvider: { type: String, required: true, unique: true },
-  wifiPassword: { type: String, required: true, unique: true },
-  wifiSpeed: { type: String, required: true, unique: true }
+  wifiProvider: { type: String, required: true, unique: false },
+  wifiPassword: { type: String, required: true, unique: false },
+  wifiSpeed: { type: String, required: true, unique: false }
 });
 
 wifiDetailsSchema.plugin(uniqueValidator);
